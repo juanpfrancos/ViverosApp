217637a6c83554291843f2d961a786485064a276
Juan Pablo Franco S <jfranco@yeminus.com>
2024-11-19 19:59:08 -0500

    Commit 1: Crear modelos para los Productos de Control

--- ViverosApp/productores/models.py
+++ ViverosApp/productores/models.py
@@ -37,3 +37,41 @@ class Labor(models.Model):
 
     def __str__(self):
         return f"Labor en {self.vivero.codigo} - {self.fecha}: {self.descripcion[:30]}"
+
+
+class ProductoControl(models.Model):
+    REGISTRO_TIPO_CHOICES = [
+        ('hongo', 'Producto Control de Hongo'),
+        ('plaga', 'Producto Control de Plaga'),
+        ('fertilizante', 'Producto Control de Fertilizante'),
+    ]
+    registro_ica = models.CharField(max_length=20, unique=True)
+    nombre = models.CharField(max_length=50)
+    frecuencia_aplicacion = models.CharField(max_length=50)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    tipo = models.CharField(max_length=15, choices=REGISTRO_TIPO_CHOICES)
+
+    def __str__(self):
+        return f"{self.nombre} ({self.tipo})"
+
+class ProductoControlHongo(models.Model):
+    producto = models.OneToOneField(ProductoControl, on_delete=models.CASCADE)
+    periodo_carencia = models.IntegerField()
+    hongo_afectado = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.producto.nombre} - Hongo: {self.hongo_afectado}"
+
+class ProductoControlPlaga(models.Model):
+    producto = models.OneToOneField(ProductoControl, on_delete=models.CASCADE)
+    periodo_carencia = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.producto.nombre} - Plaga"
+
+class ProductoControlFertilizante(models.Model):
+    producto = models.OneToOneField(ProductoControl, on_delete=models.CASCADE)
+    fecha_ultima_aplicacion = models.DateField()
+
+    def __str__(self):
+        return f"{self.producto.nombre} - Fertilizante"
