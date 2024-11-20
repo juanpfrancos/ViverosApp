ad23dde89ef1c68e937380a7c5371445d506757f
Juan Pablo Franco S <jfranco@yeminus.com>
2024-11-19 20:00:06 -0500

    Commit 2: Crear formularios para registrar y editar Productos de Control

--- ViverosApp/productores/forms.py
+++ ViverosApp/productores/forms.py
@@ -1,7 +1,6 @@
 # productores/forms.py
 from django import forms
-from .models import Finca, Vivero, Labor
-
+from .models import Finca, Vivero, Labor,ProductoControl, ProductoControlHongo, ProductoControlPlaga, ProductoControlFertilizante
 class FincaForm(forms.ModelForm):
     class Meta:
         model = Finca
@@ -17,3 +16,25 @@ class LaborForm(forms.ModelForm):
     class Meta:
         model = Labor
         fields = ['fecha', 'descripcion', 'vivero']
+
+
+
+class ProductoControlForm(forms.ModelForm):
+    class Meta:
+        model = ProductoControl
+        fields = ['registro_ica', 'nombre', 'frecuencia_aplicacion', 'valor', 'tipo']
+
+class ProductoControlHongoForm(forms.ModelForm):
+    class Meta:
+        model = ProductoControlHongo
+        fields = ['producto', 'periodo_carencia', 'hongo_afectado']
+
+class ProductoControlPlagaForm(forms.ModelForm):
+    class Meta:
+        model = ProductoControlPlaga
+        fields = ['producto', 'periodo_carencia']
+
+class ProductoControlFertilizanteForm(forms.ModelForm):
+    class Meta:
+        model = ProductoControlFertilizante
+        fields = ['producto', 'fecha_ultima_aplicacion']
