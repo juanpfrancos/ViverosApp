77086d55e8e50ff65767b919387c34f6a872faf2
Juan Pablo Franco S <jfranco@yeminus.com>
2024-11-19 19:43:16 -0500

    Commit 3: Implementar formulario para registrar y editar Fincas y Viveros

--- ViverosApp/productores/forms.py
+++ ViverosApp/productores/forms.py
@@ -1,8 +1,13 @@
 # productores/forms.py
 from django import forms
-from .models import Productor
+from .models import Finca, Vivero
 
-class ProductorForm(forms.ModelForm):
+class FincaForm(forms.ModelForm):
     class Meta:
-        model = Productor
-        fields = ['documento_identidad', 'nombre', 'apellido', 'telefono', 'correo']
+        model = Finca
+        fields = ['numero_catastro', 'municipio', 'productor']
+
+class ViveroForm(forms.ModelForm):
+    class Meta:
+        model = Vivero
+        fields = ['codigo', 'tipo_cultivo', 'finca']
