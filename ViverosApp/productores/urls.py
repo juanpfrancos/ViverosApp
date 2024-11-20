78c7c17c05e9553c917fd1a20aef08ef2f62cc7c
Juan Pablo Franco S <jfranco@yeminus.com>
2024-11-19 19:46:48 -0500

    Commit 4: Añadir vistas y URLs para manejar Fincas y Viveros

--- ViverosApp/productores/urls.py
+++ ViverosApp/productores/urls.py
@@ -7,4 +7,15 @@ urlpatterns = [
     path('registrar/', views.registrar_productor, name='registrar_productor'),
     path('editar/<int:pk>/', views.editar_productor, name='editar_productor'),
     path('eliminar/<int:pk>/', views.eliminar_productor, name='eliminar_productor'),
+        # Fincas
+    path('fincas/', views.listar_fincas, name='listar_fincas'),
+    path('fincas/registrar/', views.registrar_finca, name='registrar_finca'),
+    path('fincas/editar/<int:pk>/', views.editar_finca, name='editar_finca'),
+    path('fincas/eliminar/<int:pk>/', views.eliminar_finca, name='eliminar_finca'),
+    
+    # Viveros
+    path('viveros/', views.listar_viveros, name='listar_viveros'),
+    path('viveros/registrar/', views.registrar_vivero, name='registrar_vivero'),
+    path('viveros/editar/<int:pk>/', views.editar_vivero, name='editar_vivero'),
+    path('viveros/eliminar/<int:pk>/',views.eliminar_vivero, name='eliminar_vivero'),
 ]
--- ViverosApp/productores/views.py
+++ ViverosApp/productores/views.py
@@ -1,34 +1,66 @@
 # productores/views.py
 from django.shortcuts import render, redirect
-from .models import Productor
-from .forms import ProductorForm
+from .models import Productor, Finca, Vivero
+from .forms import ProductorForm, FincaForm, ViveroForm
 
-def listar_productores(request):
-    productores = Productor.objects.all()
-    return render(request, 'productores/listar.html', {'productores': productores})
+# Gestión de Fincas
+def listar_fincas(request):
+    fincas = Finca.objects.all()
+    return render(request, 'productores/listar_fincas.html', {'fincas': fincas})
 
-def registrar_productor(request):
+def registrar_finca(request):
     if request.method == 'POST':
-        form = ProductorForm(request.POST)
+        form = FincaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('listar_productores')
+            return redirect('listar_fincas')
     else:
-        form = ProductorForm()
-    return render(request, 'productores/registrar.html', {'form': form})
+        form = FincaForm()
+    return render(request, 'productores/registrar_finca.html', {'form': form})
 
-def editar_productor(request, pk):
-    productor = Productor.objects.get(pk=pk)
+def editar_finca(request, pk):
+    finca = Finca.objects.get(pk=pk)
     if request.method == 'POST':
-        form = ProductorForm(request.POST, instance=productor)
+        form = FincaForm(request.POST, instance=finca)
         if form.is_valid():
             form.save()
-            return redirect('listar_productores')
+            return redirect('listar_fincas')
     else:
-        form = ProductorForm(instance=productor)
-    return render(request, 'productores/editar.html', {'form': form, 'productor': productor})
+        form = FincaForm(instance=finca)
+    return render(request, 'productores/editar_finca.html', {'form': form, 'finca': finca})
 
-def eliminar_productor(request, pk):
-    productor = Productor.objects.get(pk=pk)
-    productor.delete()
-    return redirect('listar_productores')
+def eliminar_finca(request, pk):
+    finca = Finca.objects.get(pk=pk)
+    finca.delete()
+    return redirect('listar_fincas')
+
+# Gestión de Viveros
+def listar_viveros(request):
+    viveros = Vivero.objects.all()
+    return render(request, 'productores/listar_viveros.html', {'viveros': viveros})
+
+def registrar_vivero(request):
+    if request.method == 'POST':
+        form = ViveroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_viveros')
+    else:
+        form = ViveroForm()
+    return render(request, 'productores/registrar_vivero.html', {'form': form})
+
+def editar_vivero(request, pk):
+    vivero = Vivero.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = ViveroForm(request.POST, instance=vivero)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_viveros')
+    else:
+        form = ViveroForm(instance=vivero)
+    return render(request, 'productores/editar_vivero.html', {'form': form, 'vivero': vivero})
+
+def eliminar_vivero(request, pk):
+    vivero = Vivero.objects.get(pk=pk)
+    vivero.delete()
+    return redirect('listar_viveros')
